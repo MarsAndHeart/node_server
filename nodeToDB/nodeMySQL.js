b326b9e9e7b2b675ec9f451c6a3f4775d8fa9567
jianghai <liujh@hungraim.com>
2017-12-22 17:37:22 +0800

    查找信息，组装返回参数

--- nodeToDB/nodeMySQL.js
+++ nodeToDB/nodeMySQL.js
@@ -1,7 +1,29 @@
-var db = require('./db/nodeDB');  //引入刚才自定义的模块
+var db = require('../db/nodeDB');  //引入刚才自定义的模块
 //对数据库操作(从show_cascade 表中检索所有字段，并打印出结果)
+
+let jsonResponse = []
 db.query('select * from ldperson', [], function (results, fields) {
-  console.log(results);
-  console.log(JSON.stringify(results));
-  console.log(JSON.parse(JSON.stringify(results)));
+  // console.log(results);
+  // console.log(JSON.stringify(results));
+  // console.log(JSON.parse(JSON.stringify(results)));
+
+  const arrLdperson = JSON.parse(JSON.stringify(results));
+  jsonResponse=arrLdperson;
+
+  db.query('select * from lcaddress', [], function (results, fields) {
+    const arrLcaddress = JSON.parse(JSON.stringify(results));
+    jsonResponse.map(ele=>{
+      ele.lcaddress = arrLcaddress.find(item => item.customerno === ele.customerno)
+    });
+
+    db.query('select * from taxinfo', [], function (results, fields) {
+      const arrTaxinfo = JSON.parse(JSON.stringify(results));
+      jsonResponse.map(ele=>{
+        ele.taxinfo = arrTaxinfo.find(item => item.customerno === ele.customerno)
+      });
+
+      console.log(jsonResponse);
+
+    });
+  });
 });
