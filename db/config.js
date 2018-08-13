f6b49b5471297c7d6fc13df8e2b13d0984ac43e5
jianghai <liujh@hungraim.com>
2018-08-13 13:50:32 +0800

    delete boco

--- db/config.js
+++ db/config.js
@@ -1,9 +1,9 @@
 //a)database.config.js代码：
 //配置链接数据库参数
 module.exports = {
-  host : '127.0.0.1',
-  port : 3306,
-  database : 'db_bocommlife',
-  user : 'root',
-  password : '12345678'
+  host: "127.0.0.1",
+  port: 3306,
+  database: "db_boco",
+  user: "root",
+  password: "12345678"
 };
