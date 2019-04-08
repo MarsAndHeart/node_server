886186bef2b96dbffdd99f22595b15ff893fbe52
jianghai <liujh@hungraim.com>
2019-04-08 15:29:44 +0800

    add pmp and tms

---
+++ tms/precache-manifest.0f7d25c93b17d1e435d843fc94ba0687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "933af517d28943b8c847",
+    "url": "./static/js/runtime~main.933af517.js"
+  },
+  {
+    "revision": "e2e2bfe20e5666a5571d",
+    "url": "./static/js/main.e2e2bfe2.chunk.js"
+  },
+  {
+    "revision": "8a3fb3bbfe5a33b1d7db",
+    "url": "./static/js/5.8a3fb3bb.chunk.js"
+  },
+  {
+    "revision": "e78dc3cb1592cbc50d21",
+    "url": "./static/js/4.e78dc3cb.chunk.js"
+  },
+  {
+    "revision": "2ad952c77318f9674205",
+    "url": "./static/js/3.2ad952c7.chunk.js"
+  },
+  {
+    "revision": "dfb45a3eb0032a3b8ae8",
+    "url": "./static/js/2.dfb45a3e.chunk.js"
+  },
+  {
+    "revision": "b2b26837abc9b8161a67eeea8822526c",
+    "url": "./index.html"
+  }
+];
---
+++ tms/precache-manifest.afe2abbf4420e87e7859dcd4fc20505b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d11e273a23d702a66ce9",
+    "url": "./static/js/runtime~main.d11e273a.js"
+  },
+  {
+    "revision": "fa5ed1e5572857db2e1d",
+    "url": "./static/js/main.fa5ed1e5.chunk.js"
+  },
+  {
+    "revision": "b72571ad6102af899c5f",
+    "url": "./static/js/5.b72571ad.chunk.js"
+  },
+  {
+    "revision": "cb9f0f50d9043c9d9e96",
+    "url": "./static/js/4.cb9f0f50.chunk.js"
+  },
+  {
+    "revision": "bd98f1a201d7175484f9",
+    "url": "./static/js/3.bd98f1a2.chunk.js"
+  },
+  {
+    "revision": "6e4c64cc7b06df5fa835",
+    "url": "./static/js/2.6e4c64cc.chunk.js"
+  },
+  {
+    "revision": "890039c4fce9f194f44715597908d267",
+    "url": "./index.html"
+  }
+];
