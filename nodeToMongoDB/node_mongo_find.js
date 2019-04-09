8ac68e9d291a4ed123aa6ab97e01a2892b34f26d
jianghai <liujh@hungraim.com>
2019-04-09 14:18:56 +0800

    modify node_mongo_find.js

--- nodeToMongoDB/node_mongo_find.js
+++ nodeToMongoDB/node_mongo_find.js
@@ -20,15 +20,45 @@ MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
   //   });
 
   // 指定条件查询
-  var params = { name: '菜鸟教程' };
+  // var params = { name: '菜鸟教程' };
+  // dbo
+  //   .collection('site')
+  //   .find(params)
+  //   .toArray(function(err, result) {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     console.log(result);
+  //     db.close();
+  //   });
+
+  // 分页查询
+  // skip(x) 跳过x条数据
+  // limit(y) 指定返回y条数据
+  // dbo
+  //   .collection('site')
+  //   .find({})
+  //   .skip(4)
+  //   .limit(2)
+  //   .toArray(function(err, res) {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     console.log(res);
+  //     db.close();
+  //   });
+
+  // 对查询结果排序
+  // sort({key:1|-1}) 按key这个字段升序|降序
   dbo
     .collection('site')
-    .find(params)
-    .toArray(function(err, result) {
+    .find({})
+    .sort({ type: 1 })
+    .toArray(function(err, res) {
       if (err) {
         throw err;
       }
-      console.log(result);
+      console.log(res);
       db.close();
     });
 });
