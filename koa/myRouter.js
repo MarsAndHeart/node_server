e2d7389b50d899932e345f40b4be8c034e7b7a66
jianghai <liujh@hungraim.com>
2019-04-09 16:10:55 +0800

    modify koa/myRouter.js

--- koa/myRouter.js
+++ koa/myRouter.js
@@ -10,7 +10,7 @@ const app = new Koa();
  */
 function render(page) {
   return new Promise((resolve, reject) => {
-    let viewUrl = `./koa/view/${page}`;
+    let viewUrl = `./view/${page}`;
     fs.readFile(viewUrl, 'binary', (err, data) => {
       if (err) {
         reject(err);
